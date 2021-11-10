84afe042d22458fd18fe49fa76bff0c346a7954f
fiigmnt <fiigmnt@protonmail.com>
2021-11-10 02:14:10 -0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# jump-bot
-Jump newsfeed aggregator bot
+JUMP Discord Aggregator
+---
+
+A simple discord bot that aggregates news articles based on emoji reactions.
+Relies heavily on [Discord.js](https://discord.js.org/)
---
+++ main.js
@@ -0,0 +1,47 @@
+// ----------------------------------------------------------------------------------//
+// Main
+// JUMP Discord Bot (( BETA v0.1.0 ))
+// Fiig | November 10, 2021 | Updated:
+// ----------------------------------------------------------------------------------//
+
+const { Client, Intents } = require('discord.js');
+const {
+  channels: { newsfeed, aggregator },
+  emoji,
+  token,
+} = require('./config.json');
+
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+  ],
+  partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+});
+
+client.once('ready', () => {
+  console.log('Ready!');
+});
+
+client.on('messageReactionAdd', async (reaction, user) => {
+  try {
+    // When a reaction is received, check if the structure is partial
+    if (reaction.partial) {
+      await reaction.fetch();
+    }
+
+    const { message, _emoji } = reaction;
+    const reactedEmoji = _emoji.name
+
+    if (message.channelId === newsfeed && reactedEmoji === emoji) {
+      const channel = client.channels.cache.get(aggregator);
+      channel.send(message.content);
+    }
+  } catch (error) {
+    console.error('Something went wrong when fetching the message:', error);
+    return;
+  }
+});
+
+client.login(token);
